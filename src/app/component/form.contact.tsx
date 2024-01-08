e1e7f59f92c6aa2fd33b4fc9777a9255ae668f1f
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-01-08 11:04:14 +0700

    update quote input

--- src/app/component/form.contact.tsx
+++ src/app/component/form.contact.tsx
@@ -1,21 +1,81 @@
 'use client'
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Select, Space } from 'antd';
+import type { SelectProps } from 'antd';
+
+
+const options: SelectProps['options'] = [
+    { label: 'Renewable Energy', value: 'renewable-energy' },
+    { label: 'Mounting Screw', value: 'mounting-screw' },
+    { label: 'Alumilumn Frame', value: 'Orange' },
+    { label: 'Iron Steel Casting', value: 'Apple' },
+    { label: 'Moulding Extruded Rubber', value: 'Pear' },
+    { label: 'Orange', value: 'Orange' },
+    { label: 'Apple', value: 'Apple' },
+    { label: 'Pear', value: 'Pear' },
+    { label: 'Orange', value: 'Orange' },
+    { label: 'Orange', value: 'Orange' },
+];
+
+
+const handleChange = (value: string[]) => {
+    console.log(`selected ${value}`);
+};
+
+
+const CatogerySelection: React.FC = () => (
+    <Space style={{ width: '100%' }} direction="vertical">
+        <Select
+            mode="multiple"
+            allowClear
+            style={{ width: '100%' }}
+            placeholder="Please select"
+            defaultValue={['mounting-screw', 'renewable-energy']}
+            onChange={handleChange}
+            options={options}
+        />
+
+    </Space>
+);
+
 const FormContact = () => {
     return (<Form layout="vertical">
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <Form.Item label="Name" name="name" style={{ flex: 1, marginRight: '1rem' }}>
                 <Input placeholder="Your Name" />
             </Form.Item>
+            <Form.Item label="Company" name="company" style={{ flex: 1, marginRight: '1rem' }}>
+                <Input placeholder="Your Company" />
+            </Form.Item>
+
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Form.Item label="Phone" name="phone" style={{ flex: 1, marginRight: '1rem' }}>
+                <Input placeholder="Your Phone Number" />
+            </Form.Item>
             <Form.Item label="Email" name="email" style={{ flex: 1 }}>
                 <Input placeholder="Your Email" />
             </Form.Item>
         </div>
+        <Form.Item label="Catogery" name="catogery">
+            <CatogerySelection />
+        </Form.Item>
+        <Form.Item label="Quantity of product ordered" name="name" style={{ flex: 1, marginRight: '1rem' }}>
+            <Input placeholder="Your Name" />
+        </Form.Item>
+        <Form.Item label="Standard requirements" name="name" style={{ flex: 1, marginRight: '1rem' }}>
+            <Input placeholder="Your Name" />
+        </Form.Item>
+        <Form.Item label="Delivery port location" name="name" style={{ flex: 1, marginRight: '1rem' }}>
+            <Input placeholder="Your Name" />
+        </Form.Item>
+
+
         <Form.Item label="Message" name="message">
             <Input.TextArea placeholder="Your Message" />
         </Form.Item>
         <Form.Item>
             <Button type="primary" htmlType="submit">
-                Submit
+                Quote Now
             </Button>
         </Form.Item>
     </Form>)
