22f1413a6a798ba110b3fa12941e344f537f481c
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-01-08 17:48:06 +0700

    update slide

--- src/app/component/slide.home.tsx
+++ src/app/component/slide.home.tsx
@@ -9,17 +9,17 @@ const SlideHomeModule = () => {
 
     return (
         <div style={{ marginTop: '-116px' }}>
-            <Carousel dotPosition="left" autoplay draggable>
-                <SlideModule backgroundImage='/img/header-1.jpg' headerTitle='Renewable Energy Products' id='solar' />
-                <SlideModule backgroundImage='/img/header-2.jpg' headerTitle='Mounting Screw Products' id='screw' />
-                <SlideModule backgroundImage='/img/header-4.jpg' headerTitle='Aluminum Frame Products' id='aluminum' />
-                <SlideModule backgroundImage='/img/header-3.jpg' headerTitle='Iron Steel Casting Products' id='steel-casting' />
-                <SlideModule backgroundImage='/img/header-5.jpg' headerTitle='Moulded_Extruded Rubber Products' id='rubber' />
-                <SlideModule backgroundImage='/img/header-6.jpg' headerTitle='Plastic Pipe Products' id='wood' />
-                <SlideModule backgroundImage='/img/header-7.jpg' headerTitle='Plywood Products' id='pipes' />
-                <SlideModule backgroundImage='/img/header-8.jpg' headerTitle='Anam Cau - Palm Plate' id='palm-plate' />
-                <SlideModule backgroundImage='/img/header-9.jpg' headerTitle='Grassgreen Straw Reed' id='grass-straw' />
-            </Carousel>
+            {/* <Carousel dotPosition="left" autoplay draggable> */}
+            <SlideModule backgroundImage='/img/header-1.jpg' headerTitle='Renewable Energy Products' id='solar' />
+            <SlideModule backgroundImage='/img/header-2.jpg' headerTitle='Mounting Screw Products' id='screw' />
+            <SlideModule backgroundImage='/img/header-4.jpg' headerTitle='Aluminum Frame Products' id='aluminum' />
+            <SlideModule backgroundImage='/img/header-3.jpg' headerTitle='Iron Steel Casting Products' id='steel-casting' />
+            <SlideModule backgroundImage='/img/header-5.jpg' headerTitle='Moulded_Extruded Rubber Products' id='rubber' />
+            <SlideModule backgroundImage='/img/header-6.jpg' headerTitle='Plastic Pipe Products' id='wood' />
+            <SlideModule backgroundImage='/img/header-7.jpg' headerTitle='Plywood Products' id='pipes' />
+            <SlideModule backgroundImage='/img/header-8.jpg' headerTitle='Anam Cau - Palm Plate' id='palm-plate' />
+            <SlideModule backgroundImage='/img/header-9.jpg' headerTitle='Grassgreen Straw Reed' id='grass-straw' />
+            {/* </Carousel> */}
         </div>
 
     )
