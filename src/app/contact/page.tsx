c1b3ef2ac745a2a8bbc8f1b737e092ffb584546f
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-01-09 13:00:32 +0700

    update contact layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,8 +7,8 @@ import { StarFilled, PhoneOutlined, MailOutlined, EnvironmentOutlined } from '@a
 
 const ContactPage = () => {
     return (
-        <Layout>
-            <Row gutter={50} style={{ position: 'relative', display: 'flex', justifyContent: 'space-between', background: 'white' }}>
+        <Layout style={{ background: '#fff' }}>
+            <Row gutter={30} style={{ position: 'relative', display: 'flex', justifyContent: 'space-between', background: 'white' }}>
                 <Content style={{ maxWidth: '1440px', width: '100%', margin: '0 auto', padding: '0px 0', display: 'flex', paddingTop: '20px' }}>
                     <Col xs={24} sm={24} md={16}>
                         <Content style={{ display: 'flex', justifyContent: 'flex-start' }}>
@@ -31,31 +31,31 @@ const ContactPage = () => {
                         </div>
                     </Col>
 
-                    <Col xs={24} sm={24} md={8}>
+                    <Col xs={24} sm={24} md={8} style={{ border: 'solid 1px #0d0d0d0d', borderRadius: '8px', padding: '0 20px 0', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)' }}>
                         <h1 style={{ color: 'blue', fontWeight: '300' }}>Get quote</h1>
                         <FormContact />
                     </Col>
                 </Content>
             </Row>
-            <Row style={{ background: 'white', zIndex: '99' }}>
+            <Row style={{ background: 'white', zIndex: '99', marginTop: '20px' }}>
                 <Content style={{ maxWidth: '1440px', width: '100%', margin: '0 auto' }}>
                     <Row gutter={30} style={{ display: 'flex', justifyContent: 'space-between' }}>
                         <Col xs={24} sm={24} md={8}>
-                            <div style={{ backgroundColor: '#2234ae', backgroundImage: ' linear-gradient(315deg, #2234ae 0%, #191714 74%)', color: 'white', borderRadius: '8px', padding: '20px 50px 20px' }}>
+                            <div style={{ backgroundColor: '#2234ae', backgroundImage: ' linear-gradient(315deg, #2234ae 0%, #191714 74%)', color: 'white', borderRadius: '8px', padding: '10px 50px 10px' }}>
                                 <h3>International Sale Manager: Tracy Dang (Ms.)</h3>
                                 <p><PhoneOutlined />   Whatsapp: <Link style={{ color: 'white' }} href="tel:+84989837143">+84 (989) 837-143</Link></p>
                                 <p><MailOutlined /> Email: <Link style={{ color: 'white' }} href="mailto:Tracy@slmglobal.vn">Tracy@slmglobal.vn</Link></p>
                             </div>
                         </Col>
                         <Col xs={24} sm={24} md={8}>
-                            <div style={{ backgroundColor: '#2234ae', backgroundImage: ' linear-gradient(315deg, #2234ae 0%, #191714 74%)', color: 'white', borderRadius: '8px', padding: '20px 50px 20px' }}>
+                            <div style={{ backgroundColor: '#2234ae', backgroundImage: ' linear-gradient(315deg, #2234ae 0%, #191714 74%)', color: 'white', borderRadius: '8px', padding: '10px 50px 10px' }}>
                                 <h3>Product Director: Daniel Nguyen (Mr.)</h3>
                                 <p><PhoneOutlined />   Whatsapp:  <Link style={{ color: 'white' }} href="tel:+84979983355">+84 (979) 983-355</Link></p>
                                 <p><MailOutlined /> Email: <Link style={{ color: 'white' }} href="mailto:Daniel@slmglobal.vn">Daniel@slmglobal.vn</Link></p>
                             </div>
                         </Col>
                         <Col xs={24} sm={24} md={8}>
-                            <div style={{ backgroundColor: '#2234ae', backgroundImage: ' linear-gradient(315deg, #2234ae 0%, #191714 74%)', color: 'white', borderRadius: '8px', padding: '20px 50px 20px' }}>
+                            <div style={{ backgroundColor: '#2234ae', backgroundImage: ' linear-gradient(315deg, #2234ae 0%, #191714 74%)', color: 'white', borderRadius: '8px', padding: '10px 50px 10px' }}>
                                 <h3>Technical Manager: Victor Nguyen (Mr.)</h3>
                                 <p><PhoneOutlined />   Whatsapp: <Link style={{ color: 'white' }} href="tel:+84969862033">+84 (969) 862-033</Link></p>
                                 <p><MailOutlined />  Email: <Link style={{ color: 'white' }} href="mailto:Victor@slmglobal.vn">Victor@slmglobal.vn</Link></p>
