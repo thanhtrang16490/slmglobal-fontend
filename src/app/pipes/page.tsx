0206093f44c94595846a5092203c44be73fe6b31
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-01-11 13:16:03 +0700

    update product catogeries layout

--- src/app/pipes/page.tsx
+++ src/app/pipes/page.tsx
@@ -40,6 +40,7 @@ const PipesPage = async () => {
         </Content> */}
       </Content>
       <Layout style={{ maxWidth: '1200px', margin: '0 auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h2 style={{ fontSize: '28px' }}>Recommend products</h2>
         <Row gutter={3}>
           {products.data.map((product: Product) => (
             <Col key={product.id} xs={24} sm={24} md={12} lg={8} xl={6} xxl={6}>
