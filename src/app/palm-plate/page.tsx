7e7ee2087dad664816a334f60477325f2112830e
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-01-11 16:04:54 +0700

    update banner catogery product

--- src/app/palm-plate/page.tsx
+++ src/app/palm-plate/page.tsx
@@ -26,13 +26,13 @@ const PalmPlatePage = async () => {
   // console.log(products);
   return (
     <>
-      <Content style={{ width: '100%', position: 'relative', height: '350px', marginTop: '50px' }}>
-        <Content style={{ backgroundColor: 'rgba(0,0,0,.3)', width: '100%', height: '240px', textAlign: "center", backgroundImage: `url('img/renewable-energy.jpg')`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', left: '50%', position: 'absolute', translate: '-50% -50px' }}>
+      <Content style={{ width: '100%', position: 'relative', height: '300px', marginTop: '50px' }}>
+        <Content style={{ backgroundColor: 'rgba(0,0,0,.3)', width: '100%', height: '240px', textAlign: "center", backgroundImage: `url('img/renewable-energy.jpg')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', left: '50%', position: 'absolute', translate: '-50% -50px' }}>
           <div style={{ backgroundColor: 'rgba(0,0,0,.3)', width: '100%', height: '100%', position: 'absolute' }}>
           </div>
           <div style={{ color: 'white', zIndex: '99', position: 'absolute', left: '50%', translate: '-50% 10px' }}>
-            <h1 style={{ fontSize: '36px' }}>Anam Cau - Palm Plate</h1>
-            <h3>Discover new and trending products</h3>
+            <h1 style={{ fontSize: '42px', fontWeight: '500' }}>Anam Cau - Palm Plate</h1>
+            <h3 style={{ fontSize: '32px', fontWeight: '100' }}>Discover new and trending products</h3>
           </div>
         </Content>
         {/* <Content style={{ margin: '0 auto -77px', height: '185px', width: '1200%', maxWidth: '1200px', background: 'white', borderRadius: '8px', position: 'absolute', top: '150px', left: '50%', translate: '-50% -50px', boxShadow: '0 0 12px 0 rgba(0,0,0,.2)' }}>
