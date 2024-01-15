b55015c28a949f56381316a81d81eeda55326d9a
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-01-16 02:21:34 +0700

    update pagination

--- src/app/screw/page.tsx
+++ src/app/screw/page.tsx
@@ -1,12 +1,26 @@
+'use client';
 
-import { Row, Col, Layout } from 'antd';
-import ProductCard from '../component/product.card';
-import { Content } from 'antd/es/layout/layout';
+import { Col, Layout, Pagination, Row } from "antd";
+import ProductFooterModule from "../component/product.footer";
 import HeaderProductCatogeries from '../component/header.product.catogeries';
-import ProductFooterModule from '../component/product.footer';
+import ProductCard from "../component/product.card";
+import { useEffect, useState } from "react";
+
+
 type Product = {
   id: number;
 };
+
+type Meta = {
+  total: number;
+  pagination: any;
+};
+
+type ProductsData = {
+  data: Product[];
+  meta: Meta;
+};
+
 
 
 
@@ -20,23 +34,70 @@ const catogeries = [
 
   },
 
-
 ]
 
-const getData = async () => {
-  const res = await fetch(`${process.env.BACKEND_URL}products?filters[category][$eq]=Mounting%20Screw&populate=*`, {
-    next: { revalidate: 86400 }
+
+
+
+const ScrewPage = () => {
+  const [products, setProducts] = useState<ProductsData | null>(null);
+
+
+  const [meta, setMeta] = useState({
+    current: 1,
+    pageSize: 12,
+    total: 0,
+    pages: 0,
   });
-  if (!res.ok) {
-    throw new Error(res.statusText);
+
+  useEffect(() => {
+
+    getData();
+  }, []);
+
+
+  const getData = async () => {
+
+    const res = await fetch(`/screw/api?current=${meta.current}&pagesize=${meta.pageSize}`);
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    const data = await res.json();
+    setProducts(data);
+    setMeta({
+      current: data.meta.pagination.page,
+      pageSize: data.meta.pagination.pageSize,
+      pages: data.meta.pagination.pageCount,
+      total: data.meta.pagination.total,
+    })
+    console.log(data);
   }
-  return await res.json();
-
-}
 
 
-const ScrewPage = async () => {
-  const products = await getData();
+
+  if (!products) {
+    return <div>Loading...</div>;
+  }
+
+  const handleOnChange = async (page: number, pageSize: number) => {
+    console.log(page, pageSize);
+    const res = await fetch(`/screw/api?current=${page}&pagesize=${pageSize}`);
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    const data = await res.json();
+    setProducts(data);
+
+    setMeta({
+      current: page,
+      pageSize: pageSize,
+      pages: meta.pages,
+      total: meta.total,
+    })
+
+
+  }
+
   return (
     <>
       <HeaderProductCatogeries
@@ -50,12 +111,21 @@ const ScrewPage = async () => {
       <Layout style={{ maxWidth: '1440px', margin: '0 auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <h2 style={{ color: '#17274f', fontSize: '28px' }}>Recommend products</h2>
         <Row gutter={3}>
-          {products.data.map((product: Product) => (
+          {products && products.data && products.data.map((product: Product) => (
             <Col key={product.id} xs={24} sm={24} md={12} lg={8} xl={6} xxl={6}>
               <ProductCard product={product} />
             </Col>
           ))}
         </Row>
+        <Pagination
+          current={meta.current}
+          pageSize={meta.pageSize}
+          showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+          total={meta.total}
+          onChange={handleOnChange}
+          showSizeChanger={true}
+          style={{ margin: '20px 0' }}
+        />
       </Layout>
       <ProductFooterModule />
     </>
