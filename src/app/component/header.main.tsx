b9a7b4c7bf4099d7272421ea9818a43fb07ceec3
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2024-01-22 11:57:44 +0700

    update blog title position

--- src/app/component/header.main.tsx
+++ src/app/component/header.main.tsx
@@ -18,7 +18,7 @@ const HeaderMain = ({ hpcbackground = '', hpctitle, hpcsubtitle }: HeaderMainPro
                 <Content style={{ backgroundColor: 'rgba(0,0,0,.3)', width: '100%', height: '250px', textAlign: "center", backgroundImage: `url('${hpcbackground}')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', left: '50%', position: 'absolute', translate: '-50% -50px' }}>
                     <div style={{ backgroundImage: 'linear-gradient(to bottom, #17274f, #00000020,  #00000090)', width: '100%', height: '100%', position: 'absolute' }}>
                     </div>
-                    <div style={{ color: 'white', zIndex: '99', position: 'absolute', left: '50%', translate: '-50% 80%' }}>
+                    <div style={{ color: 'white', zIndex: '99', position: 'absolute', left: '50%', translate: '-50% 50%' }}>
                         <h1 style={{ fontSize: '42px', fontWeight: '500' }}>{hpctitle}</h1>
                         <h3 style={{ fontSize: '32px', fontWeight: '100' }}>{hpcsubtitle}</h3>
                     </div>
